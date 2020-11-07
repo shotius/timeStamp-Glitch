8f900c05809663a97ed0feb5493ecdd6c8d75a2b
Glitch (leather-maroon-share) <none>
2020-11-07 12:59:25 +0000

    🌏👩‍👩‍👦 Checkpoint ./server.js:24662156/252 ./public/style.css:24662156/89 ./views/index.html:24662156/115

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static(__dirname + "/public"));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(req, res) {
@@ -53,6 +53,5 @@ app.get("/api/timestamp/:date_string?", (req, res) => {
   }
   
   // if code cames her that meand url were specified correctly
-  res.json({ unix: d.getTime(),
-            utc: d});
+  res.json({ unix: d.getTime(), utc: d});
 });
